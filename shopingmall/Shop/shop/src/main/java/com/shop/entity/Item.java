84482095fdf69dc07752888aa7f74931601dfa8b
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2023-05-03 23:19:47 +0900

    Update Item.java

--- shopingmall/Shop/shop/src/main/java/com/shop/entity/Item.java
+++ shopingmall/Shop/shop/src/main/java/com/shop/entity/Item.java
@@ -50,7 +50,7 @@ public class Item extends BaseEntity  {
     }
 
     public void removeStock(int stockNumber){
-        int restStock = this.stockNumber - stockNumber;//1
+        int restStock = this.stockNumber - stockNumber;//2-1
         if(restStock<0){
             throw new OutOfStockException("상품의 재고가 부족 합니다. (현재 재고 수량: " + this.stockNumber + ")");//2
         }
@@ -73,5 +73,11 @@ public class Item extends BaseEntity  {
 4 상품의 재고를 증가시키는 메소드
 
  */
+
+/* 2-1 상품의 재고 수량에서 주문 후 남은 재고 수량을 구합니다
+
+2 상품의 재고가 주문 수량보다 작을 경우 재고 부족 예외를 발생시킵니다.
+
+3 주문 후 남은 재고 수량을 상품의 현재 재고 값으로 할당합니다. */
 
 //참고 자료 -> 백타불여일견 스프링부트와 쇼핑몰 with JPA 저자 변구훈님
